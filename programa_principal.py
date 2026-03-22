d7ade0d9b334312a484e302a2dbc7cc560806656
pcalderaro <pcalderaro@udesa.edu.ar>
2026-03-22 12:17:43 -0300

    Update programa_principal.py

--- programa_principal.py
+++ programa_principal.py
@@ -1,5 +1,5 @@
 import funciones_habitos
-lista = funciones_habitos.registrar_habitos()
+lista = funciones_habitos.registar_habitos()
 resultado = funciones_habitos.analizar_habitos(lista)
 print("Resumen de actividades:")
 print(resultado)
